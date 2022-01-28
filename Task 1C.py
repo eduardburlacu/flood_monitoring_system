cbf940f2669126e4f7246df5771b41ccf48fdfed
Eduard Burlacu <efb48@cam.ac.uk>
2022-01-28 01:42:38 +0000

    1ABC are done

--- Task 1C.py
+++ Task 1C.py
@@ -5,4 +5,5 @@ y=stations_within_radius(a,(52.2053, 0.1218),10)
 result=[]
 for station in y:
     result.append(station.name)
+result.sort()
 print(result)
--- Task1B.py
+++ Task1B.py
@@ -2,7 +2,10 @@ from floodsystem.stationdata import build_station_list
 from floodsystem.geo import stations_by_distance
 stations=build_station_list()
 x=stations_by_distance(stations,(52.2053, 0.1218))
-ans=[]
+ans1=[]
+ans2=[]
 for i in range(10):
-    ans.append((x[i][0].name,x[i][0].town,x[i][1]))
-print(ans)
+    ans1.append((x[i][0].name,x[i][0].town,x[i][1]))
+    ans2.append((x[len(x)-i-1][0].name,x[len(x)-1-i][0].town,x[(len(x)-1-i)][1]))
+print(ans1)
+print(ans2)
