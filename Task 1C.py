632df7bc4e0b0d5d9c0be98063e0ad72053d0448
Eduard Burlacu <efb48@cam.ac.uk>
2022-01-28 01:32:21 +0000

    1C made better

--- Task 1C.py
+++ Task 1C.py
@@ -1,5 +1,8 @@
 from floodsystem.stationdata import build_station_list
 from floodsystem.geo import stations_within_radius
 a=build_station_list()
-x=stations_within_radius(a,(52.2053, 0.1218),10)
-print(x)
+y=stations_within_radius(a,(52.2053, 0.1218),10)
+result=[]
+for station in y:
+    result.append(station.name)
+print(result)
